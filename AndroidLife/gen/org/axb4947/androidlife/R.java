8bde1ff3072860acb810aa62d10f2157ac7681ec
axb4947 <axb4947@rit.edu>
2012-12-03 00:41:37 -0500

    Completed Part 1 of the tutorial.

--- AndroidLife/gen/org/axb4947/androidlife/R.java
+++ AndroidLife/gen/org/axb4947/androidlife/R.java
@@ -17,20 +17,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int about_button=0x7f080002;
-        public static final int continue_button=0x7f080000;
-        public static final int exit_button=0x7f080003;
-        public static final int menu_settings=0x7f080004;
-        public static final int new_button=0x7f080001;
+        public static final int about_button=0x7f080003;
+        public static final int about_content=0x7f080000;
+        public static final int continue_button=0x7f080001;
+        public static final int exit_button=0x7f080004;
+        public static final int menu_settings=0x7f080005;
+        public static final int new_button=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int about=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int grid=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
     }
     public static final class string {
         public static final int about_label=0x7f050004;
+        public static final int about_text=0x7f050007;
+        public static final int about_title=0x7f050006;
         public static final int app_name=0x7f050000;
         public static final int continue_label=0x7f050002;
         public static final int exit_label=0x7f050005;
--- AndroidLife/src/org/axb4947/androidlife/MainActivity.java
+++ AndroidLife/src/org/axb4947/androidlife/MainActivity.java
@@ -1,13 +1,29 @@
 package org.axb4947.androidlife;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // click-handlers for buttons
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+        case R.id.about_button:
+            Intent i = new Intent(this, AboutActivity.class);
+            startActivity(i);
+            break;
+        }
     }
 }
